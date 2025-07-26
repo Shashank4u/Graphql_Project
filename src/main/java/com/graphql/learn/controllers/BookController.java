6809843c4a7d1cc56b576ab63f6e0d0412487044
shashank4u <shashankgurjar4u@gmail.com>
2025-07-26 14:24:18 +0530

    complete project for graphQL and spring boot

--- src/main/java/com/graphql/learn/controllers/BookController.java
+++ src/main/java/com/graphql/learn/controllers/BookController.java
@@ -3,13 +3,20 @@ package com.graphql.learn.controllers;
 
 import com.graphql.learn.entities.Book;
 import com.graphql.learn.services.BookService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/books")
+
+@Controller
 public class BookController {
 
 
@@ -17,22 +24,38 @@ public class BookController {
     private BookService bookService;
 
     //create
-    @PostMapping
-    public Book create( @RequestBody Book book){
-        return this.bookService.create(book);
+    @MutationMapping
+    public Book create( @Argument BookInput book){
+        Book b=new Book();
+        b. setTitle(book.getTitle());
+        b.setDesc(book.getDesc());
+        b.setPrice(book.getPrice());
+        b.setAuthor(book.getAuthor());
+        b. setPages(book.getPages());
+        return this.bookService.create(b);
     }
 
 
     //get all
-    @GetMapping
+    @QueryMapping("allBooks")
     public List<Book> getAll() {
         return this.bookService.getAll();
     }
 
     // get single book
-    @GetMapping("/{bookId}")
-    public Book get( @PathVariable int bookId){
+    @QueryMapping("getBook")
+    public Book get( @Argument int bookId){
         return this.bookService.get(bookId);
     }
 
+}
+
+@Getter
+@Setter
+class BookInput{
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
 }
